04ebfe6b38bac8a30cf746c710d05756802ae961
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-09 21:43:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,7 +6,8 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-
+counter = 0 
+while counter < coupons. validates_length 
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
